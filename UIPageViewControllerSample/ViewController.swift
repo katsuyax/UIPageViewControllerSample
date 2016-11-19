e162062d94dbf72eb596471e923667cb6e27c543
Katsuya Kato <crossbridge.kato@gmail.com>
2016-11-19 20:05:49 +0900

    Swift3対応

--- UIPageViewControllerSample/ViewController.swift
+++ UIPageViewControllerSample/ViewController.swift
@@ -8,7 +8,7 @@
 import UIKit
 
 class ViewController: UIViewController, UIPageViewControllerDataSource {
- 
+    
     // ContainerView に Embed した UIPageViewController のインスタンスを保持する
     var pageViewController: UIPageViewController?
     
@@ -20,7 +20,7 @@ class ViewController: UIViewController, UIPageViewControllerDataSource {
         
         // Storyboard 上に配置した ViewController(StoryboardID = DataViewController) をインスタンス化して配列に追加する
         for index in 0...9 {
-            let vc = storyboard?.instantiateViewControllerWithIdentifier("DataViewController") as! DataViewController
+            let vc = storyboard?.instantiateViewController(withIdentifier: "DataViewController") as! DataViewController
             vc.labelStr = index.description
             vcArray.append(vc)
         }
@@ -32,42 +32,42 @@ class ViewController: UIViewController, UIPageViewControllerDataSource {
         pageViewController!.dataSource = self
         
         // 最初に表示する画面として配列の先頭の ViewController を設定する
-        pageViewController!.setViewControllers([vcArray[0]], direction: .Forward, animated: false, completion: nil)
-
+        pageViewController!.setViewControllers([vcArray[0]], direction: .forward, animated: false, completion: nil)
+        
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
     }
-
+    
     // ボタンがタップされたらそれぞれ 0ページ、5ページ、9ページ目にジャンプさせる
-    @IBAction func handleButton(sender: UIButton) {
+    @IBAction func handleButton(_ sender: UIButton) {
         switch sender.tag {
         case 0:
-            pageViewController!.setViewControllers([vcArray[0]], direction: .Forward, animated: false, completion: nil)
+            pageViewController!.setViewControllers([vcArray[0]], direction: .forward, animated: false, completion: nil)
         case 1:
-            pageViewController!.setViewControllers([vcArray[5]], direction: .Forward, animated: false, completion: nil)
+            pageViewController!.setViewControllers([vcArray[5]], direction: .forward, animated: false, completion: nil)
         case 2:
-            pageViewController!.setViewControllers([vcArray[9]], direction: .Forward, animated: false, completion: nil)
+            pageViewController!.setViewControllers([vcArray[9]], direction: .forward, animated: false, completion: nil)
         default:
             break
         }
     }
-
+    
     // 逆方向にページ送りした時に呼ばれるメソッド
-    func pageViewController(pageViewController: UIPageViewController, viewControllerBeforeViewController viewController: UIViewController) -> UIViewController? {
+    func pageViewController(_ pageViewController: UIPageViewController, viewControllerBefore viewController: UIViewController) -> UIViewController? {
         
-        guard let index = vcArray.indexOf(viewController as! DataViewController) where index > 0 else {
+        guard let index = vcArray.index(of: viewController as! DataViewController), index > 0 else {
             return nil
         }
         
         return vcArray[index - 1]
     }
-
+    
     // 順方向にページ送りした時に呼ばれるメソッド
-    func pageViewController(pageViewController: UIPageViewController, viewControllerAfterViewController viewController: UIViewController) -> UIViewController? {
+    func pageViewController(_ pageViewController: UIPageViewController, viewControllerAfter viewController: UIViewController) -> UIViewController? {
         
-        guard let index = vcArray.indexOf(viewController as! DataViewController) where index < vcArray.count - 1 else {
+        guard let index = vcArray.index(of: viewController as! DataViewController), index < vcArray.count - 1 else {
             return nil
         }
         
